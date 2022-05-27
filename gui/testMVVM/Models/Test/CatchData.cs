f0f6fae3d4063d358b0df6183bdb968bb25b875f
Никита <kommunist.bear@gmail.com>
2022-05-27 21:36:41 +0300

    add another tabcontrol for tables

---
+++ gui/testMVVM/Models/Test/CatchData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testMVVM.Models.Test
+{
+    internal class CatchData 
+    {
+        public List<Catch> Data { get; set; }
+
+
+        public CatchData(List<Catch> data=null)
+        {
+            Data = data;
+        }
+    }
+}
