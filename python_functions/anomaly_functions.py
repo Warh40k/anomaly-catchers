9ded0521d58f888f873e72017e5eea942db6fdc4
pavlik-tikhomirov <tpa007@campus.mephi.ru>
2022-05-29 05:35:21 +0300

    add 3 anomal;y

--- python_functions/anomaly_functions.py
+++ python_functions/anomaly_functions.py
@@ -66,3 +66,50 @@ def duplicate_anomaly(ext1, ext2, txt_file = 'anomaly.txt'):
     return df1[['id_ves', 'id_own', 'date_fishery']].to_json(), len(df1)
 
     duplicate, anomaly_amount
+
+def vsd_absence_anomaly(Ext, Ext2, catch, txt_file='anomaly.txt'):
+    A = Ext[['id_own','id_vsd','date_fishery']]
+    B = Ext2[['id_vsd','id_fish','date_vsd','fish']]
+    db2 = A.merge (B, on='id_vsd', how='inner')
+    db2_fish = db2[['id_own','id_fish']].groupby(['id_own'])
+    db2_fish = db2_fish.aggregate(lambda x: x.unique().tolist())
+
+    db1_fish = catch[['id_own','id_fish']].groupby(['id_own'])
+    db1_fish = db1_fish.aggregate(lambda x: x.unique().tolist())
+    db12 = db1_fish.merge(db2_fish, on='id_own')
+    db12['div_sub_vsd'] = None
+    db12['div_sub_catch'] = None
+    for i in (db12.index):
+        db12.loc[i]['div_sub_vsd'] = set(db12.id_fish_y.loc[i]) - set(db12.id_fish_x.loc[i])
+        db12.loc[i]['div_sub_catch'] = set(db12.id_fish_x.loc[i]) - set(db12.id_fish_y.loc[i])
+    bad_fish_catch = db12[db12['div_sub_catch'] != set()]
+    bad_fish_catch = pd.DataFrame({'id_own': bad_fish_catch.index, 'id_bad_fish':bad_fish_catch['div_sub_catch'].values})
+    proverka_catch = catch[['id_fish','id_own','id_ves', 'date', 'catch_volume']]
+    proverka_catch = proverka_catch[['id_ves','id_fish','id_own']].groupby(['id_ves'])
+    proverka_catch = proverka_catch.aggregate(lambda x: x.unique().tolist())
+    check = [] # Найденная аномальная Информация id_ves, date, catch_volume
+    for i in range(len(bad_fish_catch)): #Проходим всех юр лиц len(bad_fish_catch)
+        id_own = bad_fish_catch.iloc[i]['id_own'] #id_own - это один id со стороны изсветных
+    
+    for j in range(len(proverka_catch)): #Проходим все судна
+        own = proverka_catch.iloc[j]['id_own'] #Набор принадлежностей судна к юр лицам
+
+        if id_own in own: #Проверка принаждлежности судна к подозрительному юр лицу
+            id_fish = bad_fish_catch.iloc[i]['id_bad_fish'] #id_fish - набор неправильной рыбы      
+            fish = proverka_catch.iloc[j]['id_fish']
+
+        for k_fish in id_fish: # Проверка добычи судна подозрительной рыбы
+            if(k_fish in fish):
+                ves = proverka_catch.index[j]
+                check.append([ves, id_own, k_fish])
+
+    df_check = pd.DataFrame(check)
+    df_check.rename(columns={0:'id_ves', 1:'id_own', 2:'id_fish'}, inplace=True)
+
+    with open(txt_file, 'w', encoding='utf8') as f:
+        print('Отчёт об аномалиях vsd_absence\n', file=f)
+        print(f'Выявлено {len(df_check)} аномалий (отсутствие ВСД на вылов)\n', file=f)
+        print(f'Владельцам следующих кораблей были отправлены предупреждения:\n', file=f)
+        print(df_check[['id_own', 'id_ves', 'id_fish']].sort_values(by=['id_own', 'id_ves']).value_counts().to_string(), file=f) 
+
+    return df_check.to_json(), len(df_check)
